c794aa99ce6626618eb3cc454b372e97129313d8
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-05-03 19:19:35 +0300

    Change folder name

---
+++ Conditional Statements Advanced - Exercise/New House/New House.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfFlower = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            
+            double price = 0.0;
+
+            switch (typeOfFlower)
+            {
+                case "Roses":
+                    price = count * 5.00;
+                    if (count > 80)
+                    {
+                        price = price - price * 0.10; 
+                    }
+                    
+                    break;
+                case "Dahlias":
+                    price = count * 3.80;
+                    if (count > 90)
+                    {
+                        price = price - price *0.15;
+                    }
+                    
+                    break;
+                case "Tulips":
+                    price = count * 2.80;
+                    if (count > 80)
+                    {
+                        price = price - price * 0.15;
+                    }
+                    
+                    break;
+                case "Narcissus":
+                    price = count * 3.00;
+                    if (count < 120)
+                    {
+                        price = price * 1.15;
+                    }
+                    
+                    break;
+                case "Gladiolus":
+                    price = count * 2.50;
+                    if (count < 80)
+                    {
+                        price = price * 1.20;
+                    }
+                    break;
+
+            }
+
+            if (budget >= price)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {count} {typeOfFlower} and {budget - price:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {price - budget:f2} leva more.");
+            }
+
+        }
+        
+    }
+}
---
+++ Conditional Statements Advanced - Exercise/Operations Between Numbers/Operations Between Numbers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations_Between_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1 = int.Parse(Console.ReadLine());
+            int N2 = int.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+
+            //Събиране(+), Изваждане(-), Умножение(*), Деление(/) и Модулно деление(%).
+            if (N2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N1} by zero");
+                return;
+            }
+
+            double sum = 0;
+            if (operation == '+' || operation == '-' || operation == '*')
+            {
+                if (operation == '+')
+                {
+                    sum = N1 + N2;
+                    
+                }
+                else if (operation == '-')
+                {
+                    sum = N1 - N2;
+                }
+                else
+                {
+                    sum = N1 * N2;
+                }
+                if (sum % 2 == 0)
+                {
+                    Console.WriteLine($"{N1} {operation} {N2} = {sum} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{N1} {operation} {N2} = {sum} - odd");
+                }
+            }
+            else if (operation == '/')
+            {
+                sum = 1.00 * N1 / N2;
+                Console.WriteLine($"{N1} / {N2} = {sum:f2}");
+            }
+            else if (operation == '%')
+            {
+                sum = N1 % N2;
+                Console.WriteLine($"{N1} % {N2} = {sum}");
+            }
+            
+        }
+    }
+}
